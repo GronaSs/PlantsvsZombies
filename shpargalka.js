b4ebfe5d1c28295853350e0377d25d770d674e33
GronaSs <51305157+GronaSs@users.noreply.github.com>
2019-06-03 11:49:49 +0900

    Add files via upload

---
+++ shpargalka.js
@@ -0,0 +1,44 @@
+let num = 100; //переменная- число
+let text ="text"; //переменная-текст
+let object = { //переменная - объект
+	key1:"что-то" ,//ключ со значением
+	key2:"что-то еще"
+onclick = function(){} // клики(нажатие) на различные html элементы
+}
+style.height = "100px"; //меняет различные css свойства элементов
+document.querySelector() // находит нужный элемент в html по классу 
+alert() //всплывающее окно на странице
+innerHTML = num; //заменяет содержимое элемента
+document.querySelectorAll() //находит все элементы с одинаковым классом
+	ff [0].onclick = function(){
+		tt[1].style.height = "150px";
+		tt[1].style.width = "150px";
+		tt[3].style.height = "150px";
+		tt[3].style.width = "150px";
+		tt[5].style.height = "150px";
+		tt[5].style.width = "150px";
+		tt[7].style.height = "150px";
+		tt[7].style.width = "150px";
+		tt[9].style.height = "150px";
+		tt[9].style.width = "150px";
+	}
+	ff [1].onclick = function(){
+		tt[0].style.height = "150px";
+		tt[0].style.width = "150px";
+		tt[2].style.height = "150px";
+		tt[2].style.width = "150px";
+		tt[4].style.height = "150px";
+		tt[4].style.width = "150px";
+		tt[6].style.height = "150px";
+		tt[6].style.width = "150px";
+		tt[8].style.height = "150px";
+		tt[8].style.width = "150px";
+		tt[10].style.height = "150px";
+		tt[10].style.width = "150px";
+	}
+			tt[0].style.width = "150px";
+		tt[2].style.width = "150px";
+		tt[4].style.width = "150px";
+		tt[6].style.width = "150px";
+		tt[8].style.width = "150px";
+		tt[10].style.width = "150px";
